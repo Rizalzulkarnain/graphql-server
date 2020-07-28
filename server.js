fc6f2c5769114b7922c904dbf8bfff38e3658dad
Rizal Zulkarnain <resetdariawal@gmail.com>
2020-07-28 22:54:09 +0700

    mutation graphql

--- server.js
+++ server.js
@@ -1,9 +1,13 @@
 const { ApolloServer, gql } = require('apollo-server');
-const dotenv = require('dotenv');
+const { GraphQLScalarType } = require('graphql');
+const { Kind } = require('graphql/language');
 
+const dotenv = require('dotenv');
 dotenv.config();
 
 const typeDefs = gql`
+  scalar Date
+
   enum Status {
     BEST_SELLER
     POPULAR
@@ -13,7 +17,7 @@ const typeDefs = gql`
 
   type Color {
     id: ID!
-    nameColor: String
+    name: String
   }
 
   type Store {
@@ -21,6 +25,7 @@ const typeDefs = gql`
     product: String!
     price: Int!
     rating: Int
+    createdAt: Date
     status: Status
     color: [Color]
     #fake: float
@@ -31,7 +36,26 @@ const typeDefs = gql`
     stores: [Store]
     store(id: ID): Store
   }
+
+  type Mutation {
+    addStore(product: String, price: Int, createdAt: Date, id: ID): [Store]
+  }
 `;
+
+const colors = [
+  {
+    id: 1,
+    name: 'black',
+  },
+  {
+    id: 2,
+    name: 'white',
+  },
+  {
+    id: 3,
+    name: 'grey',
+  },
+];
 
 const stores = [
   {
@@ -39,10 +63,10 @@ const stores = [
     product: 'UPS APC',
     price: 250000,
     rating: 5,
+    createdAt: new Date('11-02-2019'),
     color: [
       {
         id: 1,
-        nameColor: 'black',
       },
     ],
   },
@@ -51,10 +75,10 @@ const stores = [
     product: 'UPS ICA',
     price: 150000,
     rating: 5,
+    createdAt: new Date('05-07-2019'),
     color: [
       {
         id: 2,
-        nameColor: 'white',
       },
     ],
   },
@@ -63,10 +87,10 @@ const stores = [
     product: 'UPS EATON 5PX',
     price: 1500,
     rating: 5,
+    createdAt: new Date('03-12-2019'),
     color: [
       {
         id: 3,
-        nameColor: 'grey',
       },
     ],
   },
@@ -84,6 +108,45 @@ const resolvers = {
       return foundStore;
     },
   },
+
+  Store: {
+    color: (obj, arg, context) => {
+      //DB call
+      const colorIds = obj.color.map((color) => color.id);
+      const filteredColors = colors.filter((color) => {
+        return colorIds.includes(color.id);
+      });
+      return filteredColors;
+    },
+  },
+
+  Mutation: {
+    addStore: (obj, { id, product, price, createdAt }, context) => {
+      const newStoreList = [
+        ...stores,
+        // new store data
+        { id, product, price, createdAt },
+      ];
+      return newStoreList;
+    },
+  },
+
+  Date: new GraphQLScalarType({
+    name: 'Date',
+    description: 'created date of product upload',
+    parseValue(value) {
+      return new Date(value);
+    },
+    serialize(value) {
+      return value.getTime();
+    },
+    parseLiteral(ast) {
+      if (ast.kind === Kind.INT) {
+        return new Date(ast.value);
+      }
+      return null;
+    },
+  }),
 };
 
 const server = new ApolloServer({
