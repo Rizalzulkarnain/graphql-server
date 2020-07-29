cffb45e704f29da7af5ea7882797af416711720a
Rizal Zulkarnain <resetdariawal@gmail.com>
2020-07-29 17:06:58 +0700

    mutations and query with database

--- server.js
+++ server.js
@@ -1,11 +1,36 @@
 const { ApolloServer, gql } = require('apollo-server');
 const { GraphQLScalarType } = require('graphql');
 const { Kind } = require('graphql/language');
-
 const dotenv = require('dotenv');
+const mongoose = require('mongoose');
+
 dotenv.config();
 
+mongoose.connect(process.env.MONGO_URI, {
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useFindAndModify: true,
+  useUnifiedTopology: true,
+});
+const db = mongoose.connection;
+
+const storeSchema = new mongoose.Schema({
+  product: String,
+  price: Number,
+  rating: Number,
+  createdAt: Date,
+  status: String,
+  colorIds: [String],
+});
+
+const Store = mongoose.model('Store', storeSchema);
+
 const typeDefs = gql`
+  fragment Meta on Store {
+    createdAt
+    rating
+  }
+
   scalar Date
 
   enum Status {
@@ -37,8 +62,22 @@ const typeDefs = gql`
     store(id: ID): Store
   }
 
+  input ColorInput {
+    id: ID
+  }
+
+  input StoreInput {
+    id: ID
+    product: String
+    price: Int
+    rating: Int
+    createdAt: Date
+    status: Status
+    color: [ColorInput]
+  }
+
   type Mutation {
-    addStore(product: String, price: Int, createdAt: Date, id: ID): [Store]
+    addStore(store: StoreInput): [Store]
   }
 `;
 
@@ -98,14 +137,23 @@ const stores = [
 
 const resolvers = {
   Query: {
-    stores: () => {
-      return stores;
+    stores: async () => {
+      try {
+        const allStores = await Store.find();
+        return allStores;
+      } catch (error) {
+        console.log(error);
+        return [];
+      }
     },
-    store: (obj, { id }, context, info) => {
-      const foundStore = stores.find((store) => {
-        return store.id === id;
-      });
-      return foundStore;
+    store: async (obj, { id }) => {
+      try {
+        const foundStore = await Store.findById(id);
+        return foundStore;
+      } catch (error) {
+        console.error(error);
+        return {};
+      }
     },
   },
 
@@ -121,19 +169,27 @@ const resolvers = {
   },
 
   Mutation: {
-    addStore: (obj, { id, product, price, createdAt }, context) => {
-      const newStoreList = [
-        ...stores,
-        // new store data
-        { id, product, price, createdAt },
-      ];
-      return newStoreList;
+    addStore: async (obj, { store }, { userId }) => {
+      try {
+        if (userId) {
+          //Do mutation and of database stuff
+          await Store.create({
+            ...store,
+          });
+          const allStores = await Store.find();
+          return allStores;
+        }
+        return stores;
+      } catch (error) {
+        console.error(error);
+        return [];
+      }
     },
   },
 
   Date: new GraphQLScalarType({
     name: 'Date',
-    description: 'created date of product upload',
+    description: 'creating date of product upload',
     parseValue(value) {
       return new Date(value);
     },
@@ -154,9 +210,22 @@ const server = new ApolloServer({
   resolvers,
   introspection: true,
   playground: true,
+  context: ({ req }) => {
+    const fakeUser = {
+      userId: "HelloI'mUser",
+    };
+    return {
+      ...fakeUser,
+    };
+  },
 });
-server
-  .listen({
-    port: process.env.PORT || 5000,
-  })
-  .then(({ url }) => console.log(`Server started at ${url}`));
+
+db.on('error', console.error.bind(console, 'connection error'));
+db.once('open', function () {
+  console.log('database connected...');
+  server
+    .listen({
+      port: process.env.PORT || 5000,
+    })
+    .then(({ url }) => console.log(`Server started at ${url}`));
+});
